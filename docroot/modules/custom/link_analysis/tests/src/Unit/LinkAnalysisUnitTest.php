e59e1210b766ed26c3ac1acb20188486b8dff8c6
Jeff Fortune <jfortune@jfortune-l1.attlocal.net>
2019-12-17 08:34:48 -0500

    Fix comments in test case

--- docroot/modules/custom/link_analysis/tests/src/Unit/LinkAnalysisUnitTest.php
+++ docroot/modules/custom/link_analysis/tests/src/Unit/LinkAnalysisUnitTest.php
@@ -4,10 +4,15 @@ namespace Drupal\Tests\link_analysis\Unit;
 
 use Drupal\Tests\UnitTestCase;
 
+/**
+ * Class LinkAnalysisUnitTest.
+ *
+ * @package Drupal\Tests\link_analysis\Unit
+ */
 class LinkAnalysisUnitTest extends UnitTestCase {
 
   /**
-   * Validate that the service loads
+   * Validate that the service loads.
    */
   public function testLinkAnalysisService() {
     $this->assertNotNull(\Drupal::service('link_analysis.store'));
